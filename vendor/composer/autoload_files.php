879614b2b4ffed0526dee5facf0609a2bf753b2b
Opa Reklama <agency@opareklama.com>
2026-07-16 07:03:34 +0600

    v1.0.10 - Remove dev dependencies to reduce plugin size

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -88,5 +88,4 @@ return array(
     '356736db98a6834f0a886b8d509b0ecd' => $vendorDir . '/thecodingmachine/safe/generated/zlib.php',
     '0174385c3be07e86008907d06ee66531' => $vendorDir . '/sabberworm/php-css-parser/src/Rule/Rule.php',
     '98aea6e41b9cb79b379b10f37ba1f0b7' => $vendorDir . '/sabberworm/php-css-parser/src/RuleSet/RuleContainer.php',
-    '9b38cf48e83f5d8f60375221cd213eee' => $vendorDir . '/phpstan/phpstan/bootstrap.php',
 );
