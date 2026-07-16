b68b9b7e6c2e3fff234802a635927e934a6a7ceb
Opa Reklama <agency@opareklama.com>
2026-07-16 06:30:44 +0600

    v1.0.2 - Fix PUC branch default to main

--- opa-booking.php
+++ opa-booking.php
@@ -3,7 +3,7 @@
  * Plugin Name: Opa Booking Engine
  * Plugin URI:  https://opareklama.lt/booking-engine
  * Description: Enterprise-grade Booking Engine for Opa Reklama.
- * Version:     1.0.1
+ * Version:     1.0.2
  * Author:      Opa Reklama
  * Author URI:  https://opareklama.lt
  * License:     GPL-3.0+
@@ -29,7 +29,7 @@ if ( ! defined( 'OPA_BOOKING_MIN_WP_VERSION' ) ) {
 
 // Define Plugin Constants
 if ( ! defined( 'OPA_BOOKING_VERSION' ) ) {
-    define( 'OPA_BOOKING_VERSION', '1.0.1' );
+    define( 'OPA_BOOKING_VERSION', '1.0.2' );
 }
 if ( ! defined( 'OPA_BOOKING_PLUGIN_DIR' ) ) {
     define( 'OPA_BOOKING_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
@@ -84,5 +84,6 @@ $myUpdateChecker = \YahnisElsts\PluginUpdateChecker\v5\PucFactory::buildUpdateCh
 	__FILE__,
 	'opa-booking-engine'
 );
+$myUpdateChecker->setBranch('main');
 
 
